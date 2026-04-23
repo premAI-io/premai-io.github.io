d648700963a2ff7d0689ea905c19dd1c2eef1cb7
g-laz77 <suhanprabhu@gmail.com>
2026-04-23 23:30:26 +0200

    Update Fluso benchmark site

--- fluso-benchmark/_next/static/chunks/app/dataset/page-963bb375b0d44612.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[107,497,580,721,974],{1338:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9664,23))}},e=>{e.O(0,[664,587,18,358],()=>e(e.s=1338)),_N_E=e.O()}]);
